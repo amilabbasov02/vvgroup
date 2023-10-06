34649bd75e24fdfe56f5944ecf75fdd743174860
amilabbasov02 <amilabbas451.19@gmail.com>
2023-10-06 18:54:36 +0400

    5 pages

--- js/main.js
+++ js/main.js
@@ -20,4 +20,19 @@ $( document ).ready(function() {
         $(".bg-images img").css("opacity","0")
         $(`.bg-images img:nth-child(${child})`).css("opacity","1")
     })
+    $("select").niceSelect()
+    // HOME FORM
+    $(".form_send").click(function(){
+        $(".pop-up").css("display","flex")
+    })
+    //HOME FORM
+    $(".pop-close").click(function(){
+        $(".pop-up").css("display","none")
+    })
+    $(".pop-up .bg").click(function(){
+        $(".pop-up").css("display","none")
+    })
+    $(".win-back").click(function(){
+        window.history.back()
+    })
 });
